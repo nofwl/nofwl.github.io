0f4f678ae01437a04c57e7881c62a09985026740
lencx <cxin1314@gmail.com>
2023-11-12 02:23:59 +0800

    chore: home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,25 @@ import './index.scss';
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
-      <img style={{ width: 160, borderRadius: 120 }} src={require('../images/icon.png').default} />
-      <a href="//github.com/lencx">GitHub</a>
-      <a href="//www.producthunt.com/@lencx">Product Hunt</a>
-      <span>公众号：浮之静</span>
+      <img className="logo" style={{ width: 160, borderRadius: 120 }} src={require('../images/icon.png').default} />
+      <div className="info">
+        <a href="//github.com/lencx">GitHub</a>
+        <a href="//www.producthunt.com/@lencx">Product Hunt</a>
+        <span>公众号：浮之静</span>
+      </div>
+      <div className="open-source">
+        <h3>Project:</h3>
+        <ul>
+          <li>
+            <a href="//github.com/lencx/ChatGPT" target="_blank">ChatGPT:</a>
+            ChatGPT Desktop Application (Mac, Windows and Linux)
+          </li>
+          <li>
+            <a href="//github.com/lencx/GPTHub" target="_blank">GPTHub:</a>
+            Discover the Best in Custom GPT at OpenAI's GPT Store – Your Adventure Begins Here!
+          </li>
+        </ul>
+      </div>
     </main>
   )
 }
