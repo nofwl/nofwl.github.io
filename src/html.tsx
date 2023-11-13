6f432f586ea61c29dd07ea5cb6c9828e250a5c34
lencx <cxin1314@gmail.com>
2023-11-13 13:32:47 +0800

    chore: ads

--- src/html.tsx
+++ src/html.tsx
@@ -10,6 +10,7 @@ export default function HTML(props: any) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <meta name="google-adsense-account" content="ca-pub-5066880741262012" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-HHP8559J84"></script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5066880741262012" crossOrigin="anonymous"></script>
         {props.headComponents}
